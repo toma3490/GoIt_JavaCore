48c84bac123f81f4e0ecd8d75aa01ec7fc72d904
toma3490 <toma3490@gmail.com>
2016-08-26 11:15:54 +0300

    Update Task 4

--- src/module_2/homework/Task_4.java
+++ src/module_2/homework/Task_4.java
@@ -2,9 +2,39 @@ package module_2.homework;
 
 public class Task_4 {
 
+    static int[] balances = {1200, 250, 2000, 500, 3200};
+    static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+    static void nameChecker(String ownerName, double fund){
+        boolean flag = false;
+        for (String name : ownerNames) {
+            if(ownerName.equals(name))flag = true;
+        }
+        if (flag) fundChecker(ownerName, fund);
+        else System.out.println("Name is incorrect!!");
+    }
+
+    static void fundChecker(String ownerName, double fund){
+        boolean flag = false;
+        if(fund > 0) flag = true;
+        if (flag) System.out.println(fundBalance(ownerName, fund));
+        else System.out.println("Incorrect fund");
+    }
+
     static double fundBalance(String ownerName, double fund) {
+        int index = 0;
+        for (int i = 0; i <= ownerNames.length-1; i++) {
+            if (ownerName.equals(ownerNames[i])){
+                index = i;
+            }
+        }
+        int balance = balances[index];
+        return balance + fund;
+    }
 
+    public static void main(String[] args) {
+        String ownerName = "Jane";
+        double fund = 30.56;
 
-        return 0;
+        nameChecker(ownerName, fund);
     }
 }
