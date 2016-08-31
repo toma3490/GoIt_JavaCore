8c9ddb72043ea34dbe1f90ad0d999fa6ed03609a
Toma3490 <toma3490@gmail.com>
2016-08-31 19:17:58 +0300

    Module 3, homework, Task_2 Update

--- src/module_3/homework/task_2/Adder.java
+++ src/module_3/homework/task_2/Adder.java
@@ -2,8 +2,6 @@ package module_3.homework.task_2;
 
 public class Adder extends Arithmetic{
     boolean check(Integer a, Integer b){
-        boolean flag = false;
-        if(a >= b) flag = true;
-        return flag;
+        return a >= b;
     }
 }
