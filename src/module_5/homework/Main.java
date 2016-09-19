ab073024b49d1f5c66dd631a419daa0928b8988b
Toma3490 <toma3490@gmail.com>
2016-09-19 16:46:11 +0300

    module_5, homework update

--- src/module_5/homework/Main.java
+++ src/module_5/homework/Main.java
@@ -5,19 +5,17 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
+        BookingComAPI bookingComAPI = new BookingComAPI();
+        GoogleAPI googleAPI = new GoogleAPI();
+        TripAdvisorAPI tripAdvisorAPI = new TripAdvisorAPI();
 
-        controller.requestRooms(3000, 3, "Ukraine", "Kiev");
-        controller.requestRooms(3500, 2, "Khreschatyk", "Kiev");
-        controller.requestRooms(2700, 1, "New", "Polyanutsya");
+        System.out.println(Arrays.toString(controller.requestRooms(3000, 3, "Ukraine", "Kiev")) + '\n');
+        System.out.println(Arrays.toString(controller.requestRooms(3500, 2, "Khreschatyk", "Kiev")) + '\n');
+        System.out.println(Arrays.toString(controller.requestRooms(2700, 1, "New", "Polyanutsya")) + '\n');
 
-        Room[] rooms1 = controller.requestRooms(3000, 3, "Ukraine", "Kiev");
-        System.out.println(Arrays.toString(rooms1));
-
-        Room[] rooms2 = controller.requestRooms(3500, 2, "Khreschatyk", "Kiev");
-        System.out.println(Arrays.toString(rooms2));
-
-        Room[] rooms3 = controller.requestRooms(2700, 1, "New", "Polyanutsya");
-        System.out.println(Arrays.toString(rooms3));
+        System.out.println(Arrays.toString(controller.check(bookingComAPI, googleAPI)));
+        System.out.println(Arrays.toString(controller.check(bookingComAPI, tripAdvisorAPI)));
+        System.out.println(Arrays.toString(controller.check(googleAPI, tripAdvisorAPI)));
 
     }
 }
